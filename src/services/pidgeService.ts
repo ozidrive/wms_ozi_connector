9198923aaa3858cb1e05b1c1b0ce50892b80506b
kod-it <akash.bitd@gmail.com>
2025-07-31 12:23:26 +0530

    pidge

--- src/services/pidgeService.ts
+++ src/services/pidgeService.ts
@@ -6,7 +6,7 @@ const PIDGE_BASE_URL = 'https://api.pidge.in/'; // Replace with actual base URL 
 
 
 export async function getOrderStatus(orderId: string, token: string) {
-  const token1 = getPidgeAccessToken();
+  const token1 = await getPidgeAccessToken();
   const response = await axios.get(`${PIDGE_BASE_URL}v1.0/store/channel/vendor/order/${orderId}`, {
     headers: { Authorization: `Bearer ${token1}` },
   });
@@ -15,8 +15,9 @@ export async function getOrderStatus(orderId: string, token: string) {
 
 
 export async function getRiderCurrentLocation(orderId: string, token: string) {
+  const token1 = await getPidgeAccessToken();
   const response = await axios.get(`${PIDGE_BASE_URL}/v1.0/store/channel/vendor/order/${orderId}/fulfillment/tracking`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: { Authorization: `Bearer ${token1}` },
   });
   return response.data;
 }
